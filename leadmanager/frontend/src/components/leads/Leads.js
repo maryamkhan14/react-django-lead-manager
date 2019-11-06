67f0a25c00ef2e788e1ea87ea0a56187fd98a6ba
Maryam Khan <maryam.khan@pcc.edu>
2019-11-05 17:18:21 -0800

    Added some React functionality with a couple of components

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
+
+export class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lead List</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  leads: state.leads.leads;
+};
+
+export default connect(Leads);
